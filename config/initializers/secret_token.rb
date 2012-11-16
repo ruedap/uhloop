4b1dc8c822615b9bc34ab160de67a02ff8ead8aa
ruedap <ruedap@ruedap.com>
2012-11-17 03:00:01 +0900

    Generate new project for rails 3.2.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uhloop::Application.config.secret_token = '647a6d10e3c36224df25214573046cd0d2a23402ddb6d83c57b2761064abd1a1cf95814a69f52a11a2e417ff73c628d1775e4073f75b6eba17f98dc49afbfcff'
