5a11fcfe2d080688ebccd8933d16ec7cbb5a0838
ruedap <ruedap@ruedap.com>
2012-11-19 21:55:59 +0900

    Fix bonjour message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
+  protect_from_forgery :except => :auth # stop rails CSRF protection for this action
+
   def index
     @ssut = "#{Time.now.to_i}000"  # 前身Node.jsアプリのUNIXタイムが13桁だったので
     @cint = ENV['CLICK_INTERVAL_SEC'] || 1
   end
 
   def create
-    Pusher["channel"].trigger("event", params[:text]+"<br />")
+    Pusher["channel"].trigger("message-event", params[:text])
     render :text => "success"
   end
 end
