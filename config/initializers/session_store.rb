1ed6d523716cbfedc1b91d61a2c29760ad6a7d51
ruedap <ruedap@ruedap.com>
2014-04-09 10:31:14 +0900

    Update to rails 4.1.0

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Uhloop::Application.config.session_store :cookie_store, key: '_uhloop_session'
+Rails.application.config.session_store :cookie_store, key: '_uhloop_session'
